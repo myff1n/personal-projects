0063ac942837c02b272839caadf9b1c28c848d5f
Elle <118459583+myff1n@users.noreply.github.com>
2022-12-16 08:23:21 -0700

    added some pseudocode comments, just in case

--- python/FizzBuzz.py
+++ python/FizzBuzz.py
@@ -1,4 +1,4 @@
-#write numbers 1 - 100
+#write numbers 1 - 100 // if change needed, value on right should be 1 more than desired output
 for i in range(1, 101):
   fizz, buzz = False, False
     
@@ -10,12 +10,16 @@ for i in range(1, 101):
   if i % 5 == 0: 
     buzz = True;
   
-#look at variable(s?) and output
+#look at variables and output
+  #if both fizz and buzz are true, say "FizzBuzz"
   if fizz and buzz:
     print("FizzBuzz")
+  #if only fizz is true, say "Fizz"
   elif fizz:
     print("Fizz")
+  #if only buzz is true, say "Buzz"
   elif buzz:
     print("Buzz")
+  #if neither is true, say the number
   else:
     print(i)
