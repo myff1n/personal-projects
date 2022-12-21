ff5b69cfc15fb1b91ed1a517aed2aaec864a1545
Elle <118459583+myff1n@users.noreply.github.com>
2022-12-21 11:55:50 -0700

    Update total-cost.py

--- python/total-cost.py
+++ python/total-cost.py
@@ -1,22 +1,27 @@
-import math
+import time
+import random
 
 global purchaseList
 purchaseList = []
+global totalCost
+totalCost = 0
 
 #prompt user for number of items
 print("How many items are you purchasing?")
 itemsPurchased = input("")
 
+itemsPurchased = int(itemsPurchased)
+
 #prompt user for tax
 print("How much tax? (in decimal, ex. 0.07)")
-tax = input("")
+tax = float(input(""))
 
 #prompt user for cost (to the 100ths decimal)
 itemNumber = 1
 
-while itemNumber >= itemsPurchased:
-  print("What is the cost of item", str(itemNumber), "?")
-  itemCost = input("")
+while itemNumber <= itemsPurchased:
+  print(f"What is the cost of item {itemNumber}?")
+  itemCost = float(input(""))
   
   #add tax to item cost, store in list
   itemCost = itemCost + (itemCost * tax)
@@ -25,3 +30,11 @@ while itemNumber >= itemsPurchased:
   itemNumber += 1
   
   #add up all list items and print
+for x in purchaseList:
+  totalCost += x
+
+#"load time", for fun
+print("Calculating...")
+time.sleep(random.randint(1, 5))
+  
+print(f"After tax, the total cost of your {itemsPurchased} items is: $" '{:03.2f}'.format(totalCost))
